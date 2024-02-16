b647bd6aa46e2098e9a6ac195738c3c5196666aa
Hiédi Ferreira <hiedyferreira@gmail.com>
2024-02-16 16:45:39 -0300

    Resolução exercícios 5 e 8

---
+++ Semana_04/Exe_05/numeros.js
@@ -0,0 +1,10 @@
+/*Utilize uma estrutura condicional (if,else) para verificar se um número é positivo, negativo ou zero. Exiba os valores no console.log().*/
+
+let num = -10 //Nesse exemplo fixei um número, mas, poderia ser pedido um número ao usuário!
+if(num < 0){
+    console.log(`O número ${num} é negativo!`)
+} else if(num > 0){
+    console.log(`O número ${num} é positivo!`)
+} else{
+    console.log(`Número zero!`)
+}
---
+++ Semana_04/Exe_08/parImpar.js
@@ -0,0 +1,8 @@
+/*Implemente uma função que determine se um número é par ou ímpar.*/
+
+let num = 137 //Nesse exemplo fixei um número, mas, poderia ser pedido um número ao usuário!
+if(num % 2 == 0){ //se o resto da divisão por 2 for igual a zero
+    console.log(`O número ${num} é par!`)
+} else{
+    console.log(`O número ${num} é ímpar!`)
+}
