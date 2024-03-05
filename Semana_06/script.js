ac2d1e9b5310549dffa71dfaf34b5179764ba851
Hiédi Ferreira <hiedyferreira@gmail.com>
2024-03-05 10:17:33 -0300

    Estilização

--- Semana_06/script.js
+++ Semana_06/script.js
@@ -1,5 +1,8 @@
 function hello(){
     let name = document.getElementById('id-name').value
     let texto = document.getElementById('mostra-texto')
-    texto.innerHTML = `Seja bem vindo(a), ${name}!`    
+    texto.innerHTML = `&#9996 Seja bem vindo(a), ${name}!` 
+
+    let mudaCor = document.getElementById('form')
+    mudaCor.style.backgroundColor = 'rgba(0, 255, 255, 0.300)'
 }
